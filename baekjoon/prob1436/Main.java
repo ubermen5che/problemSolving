8d19bdef35c7a3f75cb71bcaad33fbb436e008ba
ubermen5che <ubermen5ch1308@gmail.com>
2021-08-21 23:48:05 +0900

    Add prob1436(brute force)

--- baekjoon/prob1436/Main.java
+++ baekjoon/prob1436/Main.java
@@ -7,32 +7,27 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        String tripleSix = "666";
+
         int N = sc.nextInt();
-        int initN = 1;
-        int result = 666;
+        int incN = 1;
+        int i = 0;
+        Integer result = 666;
 
-
-        while(N != initN){
-            if (initN % 19 != 7){
-                result += 1000;
-                initN += 1;
-                if (initN % 19 == 7 && N == initN)
-                    result -= 6;
-            } else{
-                result -= 6;
-                if (initN == N)
-                    break;
-                for (int i = 0; i < 9; i++) {
-                    result += 1;
-                    initN += 1;
-                    if (initN == N){
-                        System.out.println(result);
-                        System.exit(0);
+        while(true){
+            if (N == incN){
+                System.out.println(result);
+                break;
+            }else {
+                result += 1;
+                if (String.valueOf(result).length() < 4)
+                    continue;
+                else {
+                    if (String.valueOf(result).contains(tripleSix)){
+                        incN++;
                     }
                 }
-                result -= 3;
             }
         }
-        System.out.println(result);
     }
 }
